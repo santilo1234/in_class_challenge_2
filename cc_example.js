ef7a8a6bf45bd3234cf1e15b40d6d6d596a0f1e0
santiago lopez <slopezarias@usf.edu>
2025-02-11 14:46:39 -0500

    Task 2

--- cc_example.js
+++ cc_example.js
@@ -12,3 +12,36 @@ function getProductsByCategory(products, category) {
 }
 
 console.log(getProductsByCategory(products, "Electronics"));
+
+
+
+
+// Task 2
+
+
+function applyDiscount(products, discountRate) {
+    return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate)
+    
+    }
+
+) )
+
+}
+
+console.log(applyDiscount(products, .1));
+
+
+function sum3(x,y,z) {
+    return x + y + z
+
+    let numbers = [1,2,3]; //iterable
+    let someObj = {x: 1, y: 2, z: 3}; //iterable
+
+    console.log(sum3(...numbers));
+
+
+    
+    
+}
